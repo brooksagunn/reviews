79502c498a5599f8e2a228f026c67a50cc92300e
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-03-02 00:21:31 -0500

    add hardcorded dynamic values for first filled out card

---
+++ index.js
@@ -0,0 +1,10 @@
+let pfp = document.querySelector(".pfp");
+let fullName = document.querySelector(".full-name");
+let title = document.querySelector(".title");
+let description = document.querySelector(".description");
+
+pfp.src = "resources/images/joker.jpg";
+fullName.innerHTML = "Arthur Fleck";
+title.innerHTML = "Comedian";
+description.innerHTML =
+  "Wanna know how I got these scars? My father was… a drinker. And a fiend. And one night he goes off crazier than usual. Mommy gets the kitchen knife to defend herself. He doesn’t like that. Not-one-bit. So – me watching – he takes the knife to her, laughing while he does it! Turns to me, and he says, “why so serious, son?” Comes at me with the knife… “Why so serious?” He sticks the blade in my mouth… “Let’s put a smile on that face!” And…Why so serious?";
