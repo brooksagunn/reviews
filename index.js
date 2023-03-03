d2d0f5a04df41142326c8d676c795b8c64594954
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-03-03 15:29:00 -0500

    Add inline arrows to click through reviews

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ const joker = [
   "Wanna know how I got these scars? My father was… a drinker. And a fiend. And one night he goes off crazier than usual. Mommy gets the kitchen knife to defend herself. He doesn’t like that. Not-one-bit. So – me watching – he takes the knife to her, laughing while he does it! Turns to me, and he says, “why so serious, son?” Comes at me with the knife… “Why so serious?” He sticks the blade in my mouth… “Let’s put a smile on that face!” And…Why so serious?",
 ];
 
-pfp.src = joker[0];
-fullName.innerHTML = joker[1];
-title.innerHTML = joker[2];
-description.innerHTML = joker[3];
+let reviews = [joker];
+
+pfp.src = reviews[0][0];
+fullName.innerHTML = reviews[0][1];
+title.innerHTML = reviews[0][2];
+description.innerHTML = reviews[0][3];
+
+// arrowRight.addEventListener("click", function() {
+// Increment reviews array selection count by one
+// });
+
+// arrowLeft.addEventListener("click", function() {
+// Increment reviews array selection count by one
+// });
